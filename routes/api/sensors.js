0330cfd33a38136ca06ba8d0efbe97a534f32ce4
Shirokone <foresai221@gmail.com>
2020-01-02 22:15:38 +0100

    papa

--- routes/api/sensors.js
+++ routes/api/sensors.js
@@ -26,13 +26,27 @@ router.post("/", (req, res) => {
 
 // @route GET api/sensors // @desc New Sensor Data // @access Public 
 router.get("/", (req, res) => {
-    Data.find({}).sort({sensorId: "asc", ssid: "asc", date: -1}).limit(200).exec((err, data) => {
+    Data.find({}).distinct("sensorId").sort({sensorId: "asc"}).exec((err, data) => {
         if(err){ 
-            res.status(500) 
+            res.status(500).send(err) 
         } 
-        if(data){ 
-            res.status(200).json(data) 
+        if(!data){ 
+            res.status(404).send()
         } 
+        let result = []
+        let itemsProcessed = 0
+        data.forEach(sensor => {
+            Data.find({sensorId: sensor.sensorId}).distinct("ssid").sort({date: -1}).exec((err, _data) => {
+                if(err){ 
+                    res.status(500).send(err) 
+                } 
+                result.push(_data)
+                itemsProcessed++
+                if(itemsProcessed == data.length){
+                    return res.status(200).json(reslut)
+                }
+            })
+        })
     }) 
 })
 
