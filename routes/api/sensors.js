9a8887cc1838fe7ff8092fca29d5a043dbc40087
Shirokone <foresai221@gmail.com>
2020-01-24 10:15:21 +0100

    fast fix

--- routes/api/sensors.js
+++ routes/api/sensors.js
@@ -6,27 +6,35 @@ var mysql = require('mysql')
 //const Data = require("../../models/Data");
 
 //MYSQL
-var connection = mysql.createConnection({
+
+  var connection = mysql.createConnection({
     host: 'hosting1900584.online.pro',
     user: '00263908_wifi_trilaterator',
     password: 'wifi_trilaterator',
     database: '00263908_wifi_trilaterator'
   })
 
-  connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-  
-    console.log('connected as id ' + connection.threadId);
-  });
+connection.connect(function(err) {
+  if (err) {
+    console.log('Connection is asleep (time to wake it up): ', err);
+    handleDisconnect();
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
+setInterval(() => {
+  connection.query('SELECT 1', (err, rows) => {
+      if (err) throw err;
+  });   
+}, 1000);
 
 // @route POST api/sensors
 // @desc New Sensor Data
 // @access Public
 router.post("/", (req, res) => {
-
+  
       const { SensorId, APs } = req.body;
       APs.forEach(ap=>{
         connection.query("INSERT INTO readings (SSID, SensorID, Rssi) VALUES ('"+ap.SSID+"','"+SensorId+"','"+ap.RSSI+"')", (err, result)=>{
@@ -44,7 +52,7 @@ router.post("/", (req, res) => {
 router.get("/", (req, res) => {
 
     let sql = "SELECT * FROM All_Full_Readings";
-    
+
     connection.query(sql,(err, result, fields)=>{
         if(err){
             return res.status(500).json(err);
